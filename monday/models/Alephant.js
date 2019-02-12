41672dd122ef3e6e45cc21f35f554da664b422a6
atteeqahmad <atteeqahmad17@gmail.com>
2019-02-11 17:16:36 -0700

    classes.

---
+++ monday/models/Alephant.js
@@ -0,0 +1,14 @@
+export default class Alephant {
+   constructor(title, color, gender) {
+      name = 'Alephant',
+         color = 'Gray',
+         gender = 'Fmale',
+         weight = '5Tons',
+   }
+    {
+   this.name = name;
+   this.color = color;
+   this.gender = gender;
+   this.weight = weight;
+}
+}
